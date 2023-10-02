f5368d8e1b8ecdc8c5c9f08586d52d2df1ceb797
jainex17 <jainexp017@gmail.com>
2023-10-03 00:32:35 +0530

    vercel deploy

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,6 @@ app.use(cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    optionSuccessStatus:200,
 }));
 
 // import routes
@@ -23,4 +22,21 @@ import usersRoute from './routes/usersRoute.js';
 
 app.use('/api/v1', usersRoute);
 
-export default app;
+app.get('/', (req, res) => {
+    res.send('working....');
+});
+
+import dbConnect from './db_connect/database.js';
+
+const PORT = process.env.PORT || 5000;
+dbConnect();
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});   
+
+// unhandle err
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Logged Error: ${err}`);
+    server.close(() => process.exit(1));
+});
